ddb55fcebdbb51ec540a393687c6c1067c3167d3
takuya178 <takuya.u1005@gmail.com>
2022-09-25 22:02:37 +0900

    test

--- src/basic/test.ts
+++ src/basic/test.ts
@@ -1,17 +1,28 @@
 export default function test() {
-  const insertionSort = (numbers: number[]): number[] => {
-    let lenNumbers = numbers.length
-    for (let i = 1; i < lenNumbers; i ++) {
-      const temp = numbers[i];
-      let j = i - 1
-      while (j >= 0 && numbers[j] > temp) {
-        numbers[j + 1] = numbers[j]
-        j -= 1
+  const partition = (numbers: number[], low: number, high: number): number => {
+    let i = low - 1
+    const pivot = numbers[high]
+    for (let j = low; j < high; j++) {
+      if (numbers[j] <= pivot) {
+        i += 1;
+        [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
       }
-      numbers[j + 1] = temp
     }
-    console.log(numbers)
-    return numbers
+    [numbers[i+1], numbers[high]] = [numbers[high], numbers[i+1]]
+    return i + 1
   }
-  insertionSort([2, 5, 1, 8, 7, 3])
+
+  const quickSort = (numbers: number[]): number[] => {
+    const _quickSort = (numbers: number[], low: number, high: number) => {
+      if (low < high) {
+        const partitionIndex = partition(numbers, low, high)
+        _quickSort(numbers, low, partitionIndex - 1)
+        _quickSort(numbers, partitionIndex + 1, high)
+      }
+    }
+    _quickSort(numbers, 0, numbers.length - 1)
+    console.log(numbers)
+    return numbers;
+  }
+  quickSort([2, 5, 1, 8, 7, 3])
 }
