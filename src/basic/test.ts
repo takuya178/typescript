6cb62a05a4c67c5273c673b9cb2593592fbfb5ed
takuya178 <takuya.u1005@gmail.com>
2022-09-26 20:47:26 +0900

    add-mergeSort

--- src/basic/test.ts
+++ src/basic/test.ts
@@ -1,28 +1,45 @@
 export default function test() {
-  const partition = (numbers: number[], low: number, high: number): number => {
-    let i = low - 1
-    const pivot = numbers[high]
-    for (let j = low; j < high; j++) {
-      if (numbers[j] <= pivot) {
-        i += 1;
-        [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
-      }
-    }
-    [numbers[i+1], numbers[high]] = [numbers[high], numbers[i+1]]
-    return i + 1
-  }
+  const mergeSort = (numbers: number[]): number[] => {
+    if (numbers.length <= 1) return numbers;
 
-  const quickSort = (numbers: number[]): number[] => {
-    const _quickSort = (numbers: number[], low: number, high: number) => {
-      if (low < high) {
-        const partitionIndex = partition(numbers, low, high)
-        _quickSort(numbers, low, partitionIndex - 1)
-        _quickSort(numbers, partitionIndex + 1, high)
+    const center = numbers.length / 2
+    const left = numbers.slice(center, numbers.length)
+    const right = numbers.slice(0, center)
+    
+    // 要素が1つになるまでslice
+    mergeSort(left);
+    mergeSort(right);
+
+    let i = 0
+    let j = 0
+    let k = 0
+
+    // 図の[4, 5]ができている前提で以下の処理を書く
+    while (i < left.length && j < right.length) {
+      if (left[i] <= right[j]) {
+        numbers[k] = left[i]
+        i += 1
+      } else {
+        numbers[k] = right[j]
+        j += 1
       }
+      k += 1
     }
-    _quickSort(numbers, 0, numbers.length - 1)
-    console.log(numbers)
-    return numbers;
+
+    // right[4, 5], left[1, 8]で8が入りきれなかったので以下の処理で追加
+    while (i < left.length) {
+      numbers[k] = left[i]
+      i += 1;
+      k += 1;
+    }
+
+    while (j < right.length) {
+      numbers[k] = right[j]
+      j += 1;
+      k += 1;
+    }
+
+    return numbers
   }
-  quickSort([2, 5, 1, 8, 7, 3])
+  mergeSort([2, 5, 1, 8, 7, 3, 9, 10])
 }
