dbd258109effd811ef587d9ea88d6bf0ad43e323
takuya178 <takuya.u1005@gmail.com>
2022-09-30 09:37:24 +0900

    add-binarySearch

--- src/algorithm-py/ninarySearch.py
+++ src/algorithm-py/ninarySearch.py
@@ -18,3 +18,16 @@ def binary_search(numbers: List[int], value: int) -> IndexNum:
     else:
       right = mid - 1
   return -1
+
+
+  def binary_search(numbers: List[int], value: int) -> IndexNum:
+    def _binary_search(numbers: List[int], value: int, left: IndexNum, right: IndexNum) -> INdexNum:
+      if left > right:
+        return -1
+
+        mid = (left + right) // 2
+        if numbers[mid] == value:
+          return mid
+        elif numbers[mid] < value:
+          return _binary_search(numbers, value, mid + 1, right)
+    return _binary_search(numbers, value, 0, len(numbers) - 1)
--- src/algorithm-ts/search/binarySearch.ts
+++ src/algorithm-ts/search/binarySearch.ts
@@ -15,4 +15,19 @@ const binarySearch = (numbers: number[], value: number) => {
   return - 1
 }
 
+// 再帰関数で書いた場合
+const binarySearch2 = (numbers: number[], value: number) => {
+  const _binary_search = (numbers: number[], value: number, left: number, right: number): number => {
+    let mid = (left + right) / 2;
+    if (numbers[mid] === value) {
+      return mid
+    } else if (numbers[mid] < value) {
+      return _binary_search(numbers, value, mid + 1, right);
+    } else {
+      return _binary_search(numbers, value, left, mid - 1);
+    }
+  }
+  return _binary_search(numbers, 4, 0, (numbers.length));
+}
+
 binarySearch([1, 2 , 3, 4, 7, 9], 7);
