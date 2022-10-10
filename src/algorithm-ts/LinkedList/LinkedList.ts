a1577d05c2a6348801193446c5f0c7142aa32c66
takuya178 <takuya.u1005@gmail.com>
2022-10-10 21:18:44 +0900

    add-ts-linked

--- src/algorithm-ts/LinkedList/LinkedList.ts
+++ src/algorithm-ts/LinkedList/LinkedList.ts
@@ -1,24 +1,64 @@
-class Link {
+class Nodes<T> {
   data: any
-  nextNode: null
-  constructor(data: any, nextNode: null) {
+  nextNode: Nodes<T> | null
+  constructor(data: any, nextNode = null) {
     this.data = data;
     this.nextNode = nextNode;
   }
-} 
+}
 
-class LinkedList {
-  head: null
-  constructor(head: null) {
-    this.head = head
+class LinkedList<T> {
+  head: null | Nodes<T>
+  constructor() {
+    this.head = null;
   }
 
-  // reverseIterative = () => {
-  //   let previous_node = null;
-  //   let currentNode = this.head;
+  appendNode = (data: any) => {
+    let newNode = new Nodes(data);
+    if (this.head === null)  this.head === newNode;
 
-  //   while(currentNode === null) {
-  //     let nextNode = currentNode.nextNode;
-  //   }
-  // }
+    let lastNode = this.head;
+    while (lastNode.nextNode === null) {
+      lastNode = lastNode.nextNode;
+    }
+    lastNode?.nextNode === newNode;
+    return newNode
+  }
+
+  insertNode = (data: any) => {
+    let newNode = new Nodes(data);
+    this.head = newNode.nextNode; // 今までの先頭のノードを新しいノードの次のノードに指定
+    this.head = newNode; // 新しいノードを先頭のノードに指定
+    return this.head;
+  }
+
+  // このメソッドを使えばappendで入力した数字を全て表示する
+  console = () => {
+    let currentNode = this.head;
+    while (currentNode === null) {
+      console.log(currentNode.nextNode);
+      currentNode = currentNode.nextNode;
+    }
+  }
+
+  remove = (data: any) => {
+    let currentNode = this.head
+    // 削除したいデータが先頭にあった場合
+    if (currentNode && currentNode.data === data) {
+      this.head === currentNode.nextNode;
+      currentNode === null;
+      return 
+    }
+    let previousNode: Nodes<T> | null = null
+    // 削除したいデータがなければ右にシフトしていく
+    while (currentNode && currentNode.data != data) {
+      previousNode = currentNode.nextNode;
+      currentNode = currentNode.nextNode;
+    }
+
+    if (currentNode === null) return;
+
+    previousNode?.nextNode = currentNode.nextNode;
+  }
 }
+
